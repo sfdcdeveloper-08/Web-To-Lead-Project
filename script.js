72b655e4ec83bca664d706ebe2d6e95d7fccf381
Shivam Kumar <kumar.shivam@salesforce.com>
2024-08-10 23:21:15 +0530

    Cosmetic changes

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function handleSave() {
+let captchaChecked = false;
+function handleSave(event) {
   debugger;
-  let inputDateVal = document.querySelector(".inputDate");
-  console.log("🚀 ~ handleSave ~ inputDateVal:", inputDateVal.value); // this date is in string format
-  let outputDateVal = document.querySelector(".outputDate");
+  if (captchaChecked) {
+    let inputDateVal = document.querySelector(".inputDate");
+    console.log("🚀 ~ handleSave ~ inputDateVal:", inputDateVal.value); // this date is in string format
+    let outputDateVal = document.querySelector(".outputDate");
 
-  let formattedDate = new Date(inputDateVal.value).toLocaleDateString("en-In");
-  outputDateVal.value = formattedDate;
+    let formattedDate = new Date(inputDateVal.value).toLocaleDateString(
+      "en-In"
+    );
+    outputDateVal.value = formattedDate;
+  } else {
+    alert("Please check reCAPTCHA box to submit the Lead");
+    event.preventDefault();
+  }
 }
 function timestamp() {
   var response = document.getElementById("g-recaptcha-response");
@@ -19,3 +27,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchaSuccess() {
+  captchaChecked = true;
+}
