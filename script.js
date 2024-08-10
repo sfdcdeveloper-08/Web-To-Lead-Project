9d8e5f0298827537ac1b787c83e8b7b6b0e4fc03
Shivam Kumar <kumar.shivam@salesforce.com>
2024-08-10 22:48:34 +0530

    RecaptchaAdded

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function handleSave() {
   let formattedDate = new Date(inputDateVal.value).toLocaleDateString("en-In");
   outputDateVal.value = formattedDate;
 }
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
